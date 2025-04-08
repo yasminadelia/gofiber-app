e03b4dbc1ea040e9c069ac47c56f1ec99565f080
yasminadelia <yasminadelia11@gmail.com>
2025-04-08 10:57:15 +0700

    chores: add .env.example

--- config/database.go
+++ config/database.go
@@ -12,11 +12,12 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable",
+    dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
 		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
 	)
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
